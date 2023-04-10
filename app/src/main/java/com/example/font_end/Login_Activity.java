559ac8ea2e9c87c0aa0a2495963283969b17b21e
Ankush <alamba570@gmail.com>
2023-04-10 13:33:05 +0530

    a

--- app/src/main/java/com/example/font_end/Login_Activity.java
+++ app/src/main/java/com/example/font_end/Login_Activity.java
@@ -2,13 +2,39 @@ package com.example.font_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Login_Activity extends AppCompatActivity {
 
+    EditText email, pass;
+    ImageView newuser, login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        newuser = findViewById(R.id.imageView12);
+        login = findViewById(R.id.imageView11);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login_Activity.this, HomeActivity.class));
+            }
+        });
+
+        newuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login_Activity.this, SignupActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/font_end/SignupActivity.java
+++ app/src/main/java/com/example/font_end/SignupActivity.java
@@ -2,12 +2,16 @@ package com.example.font_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignupActivity extends AppCompatActivity {
 
     EditText name, lname, email, password;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,13 @@ public class SignupActivity extends AppCompatActivity {
         lname = findViewById(R.id.Lname);
         email = findViewById(R.id.email);
         password = findViewById(R.id.passwordd);
+        img = findViewById(R.id.signupbtn);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignupActivity.this, HomeActivity.class));
+            }
+        });
     }
 }
