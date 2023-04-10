0f881b40e270b86bbacf5dbaff590c6afc84672b
Ankush <alamba570@gmail.com>
2023-04-10 13:23:52 +0530

    Adds Login and signup activity

--- app/src/main/java/com/example/font_end/MainActivity.java
+++ app/src/main/java/com/example/font_end/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.font_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView signup;
+    ImageView Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup = findViewById(R.id.Signup);
+        Login = findViewById(R.id.login);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , SignupActivity.class));
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , Login_Activity.class));
+            }
+        });
     }
+    }
+
+
 }
--- app/src/main/java/com/example/font_end/SignupActivity.java
+++ app/src/main/java/com/example/font_end/SignupActivity.java
@@ -3,12 +3,20 @@ package com.example.font_end;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText name, lname, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        name = findViewById(R.id.fname);
+        lname = findViewById(R.id.Lname);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.passwordd);
     }
 }
